0ba74dec802248bccb459c10f37c47ee2a5c6c62
Ade Akintide <ade@abreezy.home>
2016-01-12 22:12:22 -0500

    fourth commit

--- app.rb
+++ app.rb
@@ -20,54 +20,54 @@ get '/' do
  erb :index
 end
 
+
+#a lil code to choose random games
 get '/game' do
-  		#a lil code to choose random games
-  		randomsport = games[rand(games.size)]
-  		"My best game is #{randomsport}"
-  	end
+  randomsport = games[rand(games.size)]
+  "My best game is #{randomsport}"
+end
 
+ #contact page
+ get '/contact' do
+  @title= 'Contact'
+  erb :contact
+end
 
-    get '/contact' do
-      @title= 'Contact'
-      erb :contact
-    end
+get '/locator' do
+  @title = 'locator'
+  erb :locator
+end
+#sending email with mandrill
+post '/email' do
+  email = params["email"]
+  sender = params["name"]
+  email_body = params["comment"]
+  puts 'sending email!'
+  m = Mandrill::API.new 
+  message = { 
+   :subject=> "New customer membership submission!",  
+   :from_name=> sender,  
+   :text=> email_body,  
+   :to=>[
+    {
+      :email=> "ogera4u@yahoo.com", 
+      :name=> "ade akin"
+    }
+    ],  
+    :html=>"<html><h1><strong>#{email_body}</strong></h1></html>",  
+    :from_email=> email 
+  } 
 
-    get '/locator' do
-      @title = 'locator'
-      erb :locator
-    end
-
-    post '/email' do
-      #puts params.inspect
-      email = params["email"]
-      sender = params["name"]
-      email_body = params["comment"]
-      puts 'sending email!'
-      m = Mandrill::API.new 
-      message = { 
-       :subject=> "New customer membership submission!",  
-       :from_name=> sender,  
-       :text=> email_body,  
-       :to=>[
-        {
-          :email=> "ogera4u@yahoo.com", 
-          :name=> "ade akin"
-        }
-        ],  
-        :html=>"<html><h1><strong>#{email_body}</strong></h1></html>",  
-        :from_email=> email 
-      } 
-
-      sending = m.messages.send message 
-      puts sending
-      redirect to('/thanks')
-    end
-
-    get '/thanks' do
-     erb :thanks
-   end
-
-   get '/sign-in' do
+  sending = m.messages.send message 
+  puts sending
+  redirect to('/thanks')
+end
+#when email is successful. You get routed here
+get '/thanks' do
+ erb :thanks
+end
+#Users sign-in
+get '/sign-in' do
       #puts "my params are" + params.inspect
       puts "You are in"
       @password = params[:password]
